b31309692e2a38462d0f70c8e430ff938fb1e694
jeliaaa <julia.aleqsandre@gmail.com>
2026-05-06 16:31:59 +0400

    scr

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -32,6 +32,22 @@ export default async function LangLayout({ children, params }: Props) {
           <TawkToWidget />
         </CurrencyProvider>
         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZG73YVWM8Y"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZG73YVWM8Y');
+            `,
+          }}
+        />
+        <Script
           id="yandex-metrika"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{

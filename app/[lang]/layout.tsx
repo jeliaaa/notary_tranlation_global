413cace8f6e980216e9b598dca9b6c92f6b90a1b
jeliaaa <julia.aleqsandre@gmail.com>
2026-05-06 12:55:33 +0400

    yandex metrika

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import TawkToWidget from '@/components/TawkToWidget';
 import ScrollToTop from '@/components/ScrollToTop';
+import Script from 'next/script';
 import '../globals.css';
 
 interface Props {
@@ -30,6 +31,22 @@ export default async function LangLayout({ children, params }: Props) {
           <Footer lang={lang} />
           <TawkToWidget />
         </CurrencyProvider>
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109077745', 'ym');
+
+              ym(109077745, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
       </body>
     </html>
   );
